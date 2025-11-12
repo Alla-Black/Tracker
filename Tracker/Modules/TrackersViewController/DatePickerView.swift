394babb8aed5712d332bb7d04784a6b35f50aa55
Alla Mozharova <alik19.09@mail.ru>
2025-11-12 16:11:52 +0300

    refactor: вынести создание и настройку DatePicker в отдельный файл, настроить отображение через вью контроллер

---
+++ Tracker/Modules/TrackersViewController/DatePickerView.swift
@@ -0,0 +1,87 @@
+import UIKit
+
+final class DatePickerView: UIView {
+    // MARK: - Private Properties
+    
+    private let dateContainer = UIView()
+    private let datePicker = UIDatePicker()
+    private let dateLabel = UILabel()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupActions()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupView() {
+        
+        //MARK: - Add Subviews
+        
+        addSubview(dateContainer)
+        dateContainer.addSubview(datePicker)
+        dateContainer.addSubview(dateLabel)
+        
+        //MARK: - Configure Appearance
+        
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.datePickerMode = .date
+        datePicker.locale = Locale(identifier: "ru_RU")
+        datePicker.alpha = 0.02
+        datePicker.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        datePicker.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        
+        dateLabel.text = DateFormatterHelper.dateFormatter.string(from: datePicker.date)
+        dateLabel.textColor = UIColor(resource: .blackYP).resolvedColor(with: UITraitCollection(userInterfaceStyle: .light))
+        dateLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        dateLabel.setContentHuggingPriority(.required, for: .horizontal)
+        dateLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+        
+        dateContainer.backgroundColor = UIColor(red: 0xF0/255.0, green: 0xF0/255.0, blue: 0xF0/255.0, alpha: 1)
+        dateContainer.layer.cornerRadius = 8
+        
+        // MARK: - Constraints
+        
+        [dateContainer, dateLabel, datePicker].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        
+        NSLayoutConstraint.activate([
+            dateContainer.heightAnchor.constraint(equalToConstant: 34),
+            dateContainer.widthAnchor.constraint(greaterThanOrEqualToConstant: 77),
+            dateContainer.leadingAnchor.constraint(equalTo: leadingAnchor),
+            dateContainer.trailingAnchor.constraint(equalTo: trailingAnchor),
+            dateContainer.topAnchor.constraint(equalTo: topAnchor),
+            dateContainer.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            datePicker.leadingAnchor.constraint(equalTo: dateContainer.leadingAnchor),
+            datePicker.trailingAnchor.constraint(equalTo: dateContainer.trailingAnchor),
+            datePicker.topAnchor.constraint(equalTo: dateContainer.topAnchor),
+            datePicker.bottomAnchor.constraint(equalTo: dateContainer.bottomAnchor),
+            
+            dateLabel.leadingAnchor.constraint(equalTo: dateContainer.leadingAnchor, constant: 12),
+            dateLabel.trailingAnchor.constraint(equalTo: dateContainer.trailingAnchor, constant: -12),
+            dateLabel.topAnchor.constraint(equalTo: dateContainer.topAnchor, constant: 6),
+            dateLabel.bottomAnchor.constraint(equalTo: dateContainer.bottomAnchor, constant: -6)
+        ])
+    }
+    
+    // MARK: - Actions
+    private func setupActions() {
+        datePicker.addTarget(
+            self,
+            action: #selector(dateChanged),
+            for: .valueChanged
+        )
+    }
+    
+    @objc private func dateChanged() {
+        dateLabel.text = DateFormatterHelper.dateFormatter.string(from: datePicker.date)
+    }
+}
+
--- Tracker/Modules/TrackersViewController/TrackersViewController.swift
+++ Tracker/Modules/TrackersViewController/TrackersViewController.swift
@@ -14,9 +14,7 @@ final class TrackersViewController: UIViewController {
     private let stubContainer = UIView()
     private let titleContainer = UIView()
     
-    private let dateContainer = UIView()
-    private let datePicker = UIDatePicker()
-    private let dateLabel = UILabel()
+    private let datePickerView = DatePickerView()
     
     
     // MARK: - Lifecycle
@@ -24,155 +22,119 @@ final class TrackersViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        addViewsToScreen()
-    }
-    
-    // MARK: - Private Methods
-    
-    private func addViewsToScreen() {
         addSubviews()
         configureAppearance()
         setupConstraints()
         setupActions()
-        
-        func addSubviews() {
-            view.addSubview(titleContainer)
-            titleContainer.addSubviews([titleNameLabel, addTrackerButton, searchBar, dateContainer])
-            
-            view.addSubview(stubContainer)
-            stubContainer.addSubviews([stubImage, stubLabel])
-            
-            dateContainer.addSubviews([dateLabel, datePicker])
-            dateContainer.bringSubviewToFront(dateLabel)
-        }
-        
-        func configureAppearance() {
-            titleNameLabel.text = "Трекеры"
-            titleNameLabel.textColor = UIColor(resource: .blackYP)
-            titleNameLabel.font = UIFont.systemFont(ofSize: 34, weight: .bold)
-            titleNameLabel.textAlignment = .left
-            
-            datePicker.preferredDatePickerStyle = .compact
-            datePicker.datePickerMode = .date
-            datePicker.locale = Locale(identifier: "ru_RU")
-            datePicker.alpha = 0.02
-            datePicker.setContentHuggingPriority(.defaultLow, for: .horizontal)
-            datePicker.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-            
-            dateLabel.text = DateFormatterHelper.dateFormatter.string(from: datePicker.date)
-            dateLabel.textColor = UIColor(resource: .blackYP).resolvedColor(with: UITraitCollection(userInterfaceStyle: .light))
-            dateLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
-            dateLabel.setContentHuggingPriority(.required, for: .horizontal)
-            dateLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
-            
-            dateContainer.backgroundColor = UIColor(red: 0xF0/255.0, green: 0xF0/255.0, blue: 0xF0/255.0, alpha: 1)
-            dateContainer.layer.cornerRadius = 8
-            
-            stubImage.image = UIImage(resource: .trackersPlaceholder)
-            
-            stubLabel.text = "Что будем отслеживать?"
-            stubLabel.textColor = UIColor(resource: .blackYP)
-            stubLabel.font = UIFont.systemFont(ofSize: 12, weight: .medium)
-            stubLabel.textAlignment = .center
-            
-            var config = addTrackerButton.configuration ?? UIButton.Configuration.plain()
-            let plusImage = UIImage(resource: .addTracker).withRenderingMode(.alwaysTemplate)
-            config.image = plusImage
-            config.contentInsets = .init(top: 12, leading: 0, bottom: 12, trailing: 0)
-            config.baseForegroundColor = UIColor(resource: .blackYP)
-            addTrackerButton.configuration = config
-            addTrackerButton.imageView?.contentMode = .scaleAspectFit
-            addTrackerButton.contentHorizontalAlignment = .leading
-            
-            searchBar.placeholder = "Поиск"
-            searchBar.searchBarStyle = .minimal
-        }
-        
-        func setupConstraints() {
-            let field = searchBar.searchTextField
-            
-            [titleContainer, titleNameLabel, datePicker, addTrackerButton, searchBar, stubContainer, stubImage, stubLabel, dateContainer, dateLabel, field].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
-            
-            NSLayoutConstraint.activate([
-                titleContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-                titleContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                titleContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                titleContainer.bottomAnchor.constraint(equalTo: searchBar.topAnchor, constant: -10),
-                
-                dateContainer.heightAnchor.constraint(equalToConstant: 34),
-                dateContainer.widthAnchor.constraint(greaterThanOrEqualToConstant: 77),
-                dateContainer.trailingAnchor.constraint(equalTo: titleContainer.trailingAnchor, constant: -16),
-                dateContainer.topAnchor.constraint(equalTo: titleContainer.topAnchor, constant: 5),
-                
-                datePicker.leadingAnchor.constraint(equalTo: dateContainer.leadingAnchor),
-                datePicker.trailingAnchor.constraint(equalTo: dateContainer.trailingAnchor),
-                datePicker.topAnchor.constraint(equalTo: dateContainer.topAnchor),
-                datePicker.bottomAnchor.constraint(equalTo: dateContainer.bottomAnchor),
-                
-                dateLabel.leadingAnchor.constraint(equalTo: dateContainer.leadingAnchor, constant: 12),
-                dateLabel.trailingAnchor.constraint(equalTo: dateContainer.trailingAnchor, constant: -12),
-                dateLabel.topAnchor.constraint(equalTo: dateContainer.topAnchor, constant: 6),
-                dateLabel.bottomAnchor.constraint(equalTo: dateContainer.bottomAnchor, constant: -6),
-                
-                titleNameLabel.topAnchor.constraint(equalTo: titleContainer.topAnchor, constant: 44),
-                titleNameLabel.leadingAnchor.constraint(equalTo: titleContainer.leadingAnchor, constant: 16),
-                
-                addTrackerButton.widthAnchor.constraint(equalToConstant: 42),
-                addTrackerButton.heightAnchor.constraint(equalToConstant: 42),
-                addTrackerButton.centerYAnchor.constraint(equalTo: dateContainer.centerYAnchor),
-                addTrackerButton.leadingAnchor.constraint(equalTo: titleContainer.leadingAnchor, constant: 16),
-                
-                searchBar.leadingAnchor.constraint(equalTo: titleContainer.leadingAnchor, constant: 16),
-                searchBar.trailingAnchor.constraint(equalTo: titleContainer.trailingAnchor, constant: -16),
-                searchBar.topAnchor.constraint(equalTo: titleNameLabel.bottomAnchor, constant: 7),
-                
-                field.heightAnchor.constraint(greaterThanOrEqualToConstant: 36),
-                field.leadingAnchor.constraint(equalTo: searchBar.leadingAnchor),
-                field.trailingAnchor.constraint(equalTo: searchBar.trailingAnchor),
-                
-                stubContainer.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10),
-                stubContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-                stubContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                stubContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                
-                stubImage.widthAnchor.constraint(equalToConstant: 80),
-                stubImage.heightAnchor.constraint(equalToConstant: 80),
-                stubImage.centerXAnchor.constraint(equalTo: stubContainer.centerXAnchor),
-                stubImage.centerYAnchor.constraint(equalTo: stubContainer.centerYAnchor),
-                
-                stubLabel.centerXAnchor.constraint(equalTo: stubImage.centerXAnchor),
-                stubLabel.topAnchor.constraint(equalTo: stubImage.bottomAnchor, constant: 8),
-                stubLabel.leadingAnchor.constraint(greaterThanOrEqualTo: stubContainer.leadingAnchor, constant: 16),
-                stubLabel.trailingAnchor.constraint(lessThanOrEqualTo: stubContainer.trailingAnchor, constant: -16)
-            ])
-        }
-        
-        func setupActions() {
-            datePicker.addTarget(
-                self,
-                action: #selector(dateChanged),
-                for: .valueChanged
-            )
-            
-            addTrackerButton.addTarget(
-                self,
-                action: #selector(didTapAddTrackerButton),
-                for: .touchUpInside
-            )
-            
-            searchBar.delegate = self
-        }
     }
     
-    // MARK: - Actions
+    // MARK: - Private Methods
+    
+    // MARK: - Setup UI
+    
+    private func addSubviews() {
+        view.addSubview(titleContainer)
+        titleContainer.addSubviews([titleNameLabel, addTrackerButton, searchBar, datePickerView])
+        
+        view.addSubview(stubContainer)
+        stubContainer.addSubviews([stubImage, stubLabel])
+    }
+    
+    private func configureAppearance() {
+        titleNameLabel.text = "Трекеры"
+        titleNameLabel.textColor = UIColor(resource: .blackYP)
+        titleNameLabel.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        titleNameLabel.textAlignment = .left
+        
+        stubImage.image = UIImage(resource: .trackersPlaceholder)
+        
+        stubLabel.text = "Что будем отслеживать?"
+        stubLabel.textColor = UIColor(resource: .blackYP)
+        stubLabel.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        stubLabel.textAlignment = .center
+        
+        datePickerView.setContentHuggingPriority(.required, for: .horizontal)
+        datePickerView.setContentCompressionResistancePriority(.required, for: .horizontal)
+        
+        var config = addTrackerButton.configuration ?? UIButton.Configuration.plain()
+        let plusImage = UIImage(resource: .addTracker).withRenderingMode(.alwaysTemplate)
+        config.image = plusImage
+        config.contentInsets = .init(top: 12, leading: 0, bottom: 12, trailing: 0)
+        config.baseForegroundColor = UIColor(resource: .blackYP)
+        addTrackerButton.configuration = config
+        addTrackerButton.imageView?.contentMode = .scaleAspectFit
+        addTrackerButton.contentHorizontalAlignment = .leading
+        
+        searchBar.placeholder = "Поиск"
+        searchBar.searchBarStyle = .minimal
+    }
+    
+    // MARK: - Constraints
+    
+    private func setupConstraints() {
+        let field = searchBar.searchTextField
+        
+        [titleContainer, titleNameLabel, addTrackerButton, searchBar, stubContainer, stubImage, stubLabel, datePickerView, field].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        
+        NSLayoutConstraint.activate([
+            titleContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            titleContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            titleContainer.bottomAnchor.constraint(equalTo: searchBar.topAnchor, constant: -10),
+            
+            datePickerView.trailingAnchor.constraint(equalTo: titleContainer.trailingAnchor, constant: -16),
+            datePickerView.topAnchor.constraint(equalTo: titleContainer.topAnchor, constant: 5),
+            datePickerView.widthAnchor.constraint(greaterThanOrEqualToConstant: 77),
+            
+            
+            titleNameLabel.topAnchor.constraint(equalTo: titleContainer.topAnchor, constant: 44),
+            titleNameLabel.leadingAnchor.constraint(equalTo: titleContainer.leadingAnchor, constant: 16),
+            
+            addTrackerButton.widthAnchor.constraint(equalToConstant: 42),
+            addTrackerButton.heightAnchor.constraint(equalToConstant: 42),
+            addTrackerButton.centerYAnchor.constraint(equalTo: datePickerView.centerYAnchor),
+            addTrackerButton.leadingAnchor.constraint(equalTo: titleContainer.leadingAnchor, constant: 16),
+            
+            searchBar.leadingAnchor.constraint(equalTo: titleContainer.leadingAnchor, constant: 16),
+            searchBar.trailingAnchor.constraint(equalTo: titleContainer.trailingAnchor, constant: -16),
+            searchBar.topAnchor.constraint(equalTo: titleNameLabel.bottomAnchor, constant: 7),
+            
+            field.heightAnchor.constraint(greaterThanOrEqualToConstant: 36),
+            field.leadingAnchor.constraint(equalTo: searchBar.leadingAnchor),
+            field.trailingAnchor.constraint(equalTo: searchBar.trailingAnchor),
+            
+            stubContainer.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10),
+            stubContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stubContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stubContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            stubImage.widthAnchor.constraint(equalToConstant: 80),
+            stubImage.heightAnchor.constraint(equalToConstant: 80),
+            stubImage.centerXAnchor.constraint(equalTo: stubContainer.centerXAnchor),
+            stubImage.centerYAnchor.constraint(equalTo: stubContainer.centerYAnchor),
+            
+            stubLabel.centerXAnchor.constraint(equalTo: stubImage.centerXAnchor),
+            stubLabel.topAnchor.constraint(equalTo: stubImage.bottomAnchor, constant: 8),
+            stubLabel.leadingAnchor.constraint(greaterThanOrEqualTo: stubContainer.leadingAnchor, constant: 16),
+            stubLabel.trailingAnchor.constraint(lessThanOrEqualTo: stubContainer.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    // MARK: - Actions wiring
+    
+    private func setupActions() {
+        addTrackerButton.addTarget(
+            self,
+            action: #selector(didTapAddTrackerButton),
+            for: .touchUpInside
+        )
+        
+        searchBar.delegate = self
+    }
     
     @objc private func didTapAddTrackerButton() {
-        
+        //TODO: реализовать функционал нажатия на кнопку
     }
     
-    @objc private func dateChanged() {
-        dateLabel.text = DateFormatterHelper.dateFormatter.string(from: datePicker.date)
-    }
-
 }
 
