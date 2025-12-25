2cf10a0c357afcf58ed68f6953b2d777e3c6c33b
Alla Mozharova <alik19.09@mail.ru>
2025-12-25 16:37:19 +0300

    feat: добавить TrackerCategoryStore для работы с категориями в Core Data

--- DataStore/TrackerCategoryStore.swift
+++ DataStore/TrackerCategoryStore.swift
@@ -1,12 +1,45 @@
-import UIKit
 import CoreData
 
+// MARK: - TrackerCategoryStore
+
 final class TrackerCategoryStore {
+    // MARK: - Private Properties
+    
     private let context: NSManagedObjectContext
+    
+    // MARK: - Initializers
     
     init(context: NSManagedObjectContext) {
         self.context = context
     }
     
-    // TODO: реализовать работу с категориями после появления экрана категорий
+    // MARK: - Public Methods
+    
+    func addCategory(title: String) throws {
+        let trimmedTitle = title.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard !trimmedTitle.isEmpty else { return }
+        
+        let object = TrackerCategoryCoreData(context: context)
+        
+        object.title = trimmedTitle
+        
+        try context.save()
+    }
+    
+    func delete(_ trackerCategoryCoreData: TrackerCategoryCoreData) throws {
+        context.delete(trackerCategoryCoreData)
+        try context.save()
+    }
+    
+    func fetchCategoryTitles() -> [String] {
+        let request = NSFetchRequest<TrackerCategoryCoreData>(entityName: "TrackerCategoryCoreData")
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        do {
+            let objects = try context.fetch(request)
+            return objects.compactMap { $0.title }
+        } catch {
+            return []
+        }
+    }
 }
