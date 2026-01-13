56a3d7880ead7f987bd367e19ecf1fda9bb9a487
Alla Mozharova <alik19.09@mail.ru>
2026-01-13 17:42:28 +0300

    refactor: вынести логику создания окна и выбора стартового rootViewController в приватные методы

--- Tracker/App/SceneDelegate.swift
+++ Tracker/App/SceneDelegate.swift
@@ -1,30 +1,40 @@
 import UIKit
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let hasCompletedOnboarding = UserDefaults.standard.bool(forKey: "hasViewedOnboarding")
+        
         
         guard let windowScene = scene as? UIWindowScene else { return }
         
-        window = UIWindow(windowScene: windowScene)
+       let window = createWindow(with: windowScene)
+        window.rootViewController = makeRootViewController()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+    
+    // MARK: - Private Methods
+    
+    private func createWindow(with scene: UIWindowScene) -> UIWindow {
+        UIWindow(windowScene: scene)
+    }
+    
+    private func makeRootViewController() -> UIViewController {
+        let hasCompletedOnboarding = UserDefaults.standard.bool(forKey: "hasViewedOnboarding")
         
         if hasCompletedOnboarding {
-            window?.rootViewController = TabBarController()
+            return TabBarController()
         } else {
             let onboarding = OnboardingViewController()
-            
             onboarding.onFinish = { [weak self] in
                 self?.window?.rootViewController = TabBarController()
             }
-             
-            window?.rootViewController = onboarding
+            return onboarding
         }
         
-        window?.makeKeyAndVisible()
     }
-
 }
 
