5ac37dd9ef8ae1cede721d2ab11fe13a0c9d9a92
Alla Mozharova <alik19.09@mail.ru>
2026-01-19 13:29:12 +0300

    feat: подключить экран статистики к TabBarController

--- Tracker/UI/TabBarController/TabBarController.swift
+++ Tracker/UI/TabBarController/TabBarController.swift
@@ -4,22 +4,46 @@ final class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let trackersViewController = TrackersViewController()
-        trackersViewController.tabBarItem = UITabBarItem(
+        setupViewControllers()
+        setupTabBarAppearance()
+    }
+    
+    private func setupViewControllers() {
+        let trackersVC = makeTrackersTab()
+        let statisticsVC = makeStatisticsTab()
+        viewControllers = [trackersVC, statisticsVC]
+    }
+    
+    private func makeTrackersTab() -> UIViewController {
+        let vc = TrackersViewController()
+        vc.tabBarItem = UITabBarItem(
             title: AppStrings.MainTab.tabTrackersTitle,
             image: UIImage(resource: .trackersButton),
             selectedImage: nil
         )
+        return vc
+    }
+    
+    private func makeStatisticsTab() -> UIViewController {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            assertionFailure("AppDelegate is missing")
+            return UIViewController()
+        }
         
-        let statisticViewController = StatisticViewController()
-        statisticViewController.tabBarItem = UITabBarItem(
+        let recordStore = appDelegate.trackerRecordStore
+        let provider = StatisticsProvider(recordStore: recordStore)
+        let viewModel = StatisticsViewModel(statisticsProvider: provider)
+        let vc = StatisticsViewController(viewModel: viewModel)
+        
+        vc.tabBarItem = UITabBarItem(
             title: AppStrings.MainTab.tabStatisticsTitle,
             image: UIImage(resource: .statisticButton),
             selectedImage: nil
         )
-        
-        viewControllers = [trackersViewController, statisticViewController]
-        
+        return vc
+    }
+    
+    private func setupTabBarAppearance() {
         tabBar.layer.borderWidth = 0.5
         tabBar.layer.borderColor = UIColor(resource: .tabBarSeparator).cgColor
         tabBar.clipsToBounds = true
